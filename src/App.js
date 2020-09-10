2f6cbe69a4518dd991502a24d6e1f68f6359f95c
maxilukowski <maxi.lukowski@yahoo.com>
2020-09-10 16:01:29 +0200

    Add env

--- src/App.js
+++ src/App.js
@@ -10,28 +10,24 @@ export default function App() {
   const [playedMap, setPlayedMap] = useState('')
   const [playedHero, setPlayedHero] = useState('')
   const [winOrLoss, setWinOrLoss] = useState('')
+  const [allStats, setAllStats] = useState([])
 
+  const getUserData = async () => {
+    const { data } = await axios.get(process.env.REACT_APP_API_URL + '/stats')
+    console.log(data)
+    setAllStats(data)
+  }
   useEffect(() => {
-    axios
-      .post('http://localhost:3000/stats', {
-        firstName: 'Fred',
-        lastName: 'Flintstone',
-      })
-      .then(function (response) {
-        console.log(response)
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
-  })
-
+    getUserData()
+  }, [])
   return (
     <Container>
+      <div>
+        {allStats.map((userData) => {
+          return <p key={userData.id}>{userData.id}</p>
+        })}
+      </div>
       <KdaForm onSubmit={setAllInfo} />
-      {/* @TODO 1 Would be great: Move right and display all form values */}
-      {/* @TODO 2 udemy */}
-      {/* @TODO 3 Optional: Try to store data into json-server */}
-      {/* @TODO 4 Optional: Try to read data from json-server */}
       <DisplayStats
         kda={kda}
         map={playedMap}
@@ -41,11 +37,23 @@ export default function App() {
     </Container>
   )
 
-  function setAllInfo(formData) {
-    setPlayedHero(formData.hero)
-    setKda(formData.kda)
-    setPlayedMap(formData.map)
-    setWinOrLoss(formData.winOrLoss)
+  async function setAllInfo({ hero, kda, map, winOrLoss }) {
+    setPlayedHero(hero)
+    setKda(kda)
+    setPlayedMap(map)
+    setWinOrLoss(winOrLoss)
+    //warum geht es nicht mit dem state? soll awaiten!
+    const { data } = await axios.post(
+      process.env.REACT_APP_API_URL + '/stats',
+      {
+        id: Date.now(),
+        kda,
+        heroPicked: hero,
+        mapPlayed: map,
+        winOrLoss,
+      }
+    )
+    getUserData()
   }
 }
 
